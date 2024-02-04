0a67a1f006489b9a9399b649bc462e9d1e6b1d4a
MT <04x0xx@gmail.com>
2024-02-04 19:51:58 -0400

    Fixed Table Styles

--- src/views/Reports.js
+++ src/views/Reports.js
@@ -1,15 +1,22 @@
-import {Card, CardContainer} from "../components/card";
+import {Table, TableRow} from "../components/table";
 
 function Reports() {
 
     let amnt_of_reports = 15;
-    let reports = [];
+    let reports = [
+        <TableRow header bold>
+            <p>Name</p>
+            <p className="collapsible">Description</p>
+            <p>Description-2</p>
+        </TableRow>
+    ];
     for (let i=0; i<amnt_of_reports; i++) {
         reports.push(
-            <Card
-                title="Very Import Report Thing"
-                description="Description"
-            />
+            <TableRow href="#">
+                <p>Very Import Report Thing</p>
+                <p className="collapsible">Description</p>
+                <p>Description-2</p>
+            </TableRow>
         );
     }
 
@@ -18,20 +25,10 @@ function Reports() {
             <h1>Reports</h1>
             <hr />
 
-            <CardContainer>
+            <Table margin="5px">
                 {reports}
-            </CardContainer>
-            
-            {/*
-            <Table>
-                <TableRow>
-                    <p>1</p>
-                    <p>1</p>
-                    <p>1</p>
-                    <p>1</p>
-                </TableRow>
             </Table>
-            */}
+            
         </>
     );
 }
