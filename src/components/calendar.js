76e12e7b8b319a08c9dfdbeae916fb4fae196cbb
MT <04x0xx@gmail.com>
2024-02-14 16:17:12 -0400

    Made calendar events moveable

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,7 +1,10 @@
+import {useState, useCallback} from "react";
+
 import {Calendar as RBigCalendar, momentLocalizer} from "react-big-calendar";
-import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
-import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+
+import moment from "moment";
 
 import "./calendar.css";
 
@@ -11,34 +14,72 @@ const localizer = momentLocalizer(moment)
 // This section is going to be replaced with an API call
 const events = [
     {
+        id: 5,
         title: "An event that is scheduled for today!!",
-        start: moment(),
-        end: moment().add(5, "hours"),
-        allDay: true, // This needs to be set or else the "work week" section breaks
+        start: new Date(new Date().setHours(new Date().getHours() - 3)),
+        end: new Date(new Date().setHours(new Date().getHours() + 3)),
     },
 ];
 
 const views = {
-    "month": true,
-    "work_week": true,
     "agenda": true,
+    "month": true,
+    "week": true,
+    "day": true,
 };
 
+const allowAllDays = false;
+
 function Calendar() {
-	return (
-		<>
-			<div style={{color: "black",}}>
-				<DnDCalendar
-					localizer={localizer}
-                    events={events}
+
+    const [myEvents, setEvents] = useState(events);
+
+    const moveEvent = useCallback(
+        ({ event, start, end, isAllDay: droppedOnAllDaySlot = false }) => {
+            let allDay = event;
+
+            allDay = droppedOnAllDaySlot && allowAllDays;
+
+            setEvents((prev) => {
+                const existing = prev.find((ev) => ev.id === event.id) ?? {};
+                const filtered = prev.filter((ev) => ev.id !== event.id);
+                return [...filtered, { ...existing, start, end, allDay }];
+            })
+        },
+        [setEvents]
+    );
+
+    const resizeEvent = useCallback(
+        ({ event, start, end }) => {
+            setEvents((prev) => {
+                const existing = prev.find((ev) => ev.id === event.id) ?? {};
+                const filtered = prev.filter((ev) => ev.id !== event.id);
+                return [...filtered, { ...existing, start, end }];
+            })
+        },
+        [setEvents]
+    );
+
+    return (
+        <>
+            <div style={{color: "black",}}>
+                <DnDCalendar
+                    localizer={localizer}
+                    events={myEvents}
                     views={views}
-					draggableAccessor={ (e) => true }
-					startAccessor="start"
-					endAccessor="end"
-				/>
-			</div>
-		</>
-	);
+
+                    onEventDrop={ moveEvent }
+                    onEventResize={ resizeEvent }
+
+                    startAccessor="start"
+                    endAccessor="end"
+
+                    popup
+                    resizeable
+                />
+            </div>
+        </>
+    );
 }
 
 export {Calendar};
