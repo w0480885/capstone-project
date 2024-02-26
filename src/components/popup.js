43145fe613928669b3acfa60e19cf03e14a1110e
Tobin <W0473744@campus.nscc.ca>
2024-02-26 10:15:27 -0400

    Made button work in higher context

--- src/components/popup.js
+++ src/components/popup.js
@@ -2,29 +2,21 @@ import "./popup.css";
 import {useState} from "react";
 
 function Popup({ children, ...props}) {
-    /* props.title sets what the title of the popup is */
-
-    const [popupDisplay, setPopupDisplay] = useState("default");
-
-    function delete_self(e) {
-        setPopupDisplay("none");
-    }
+    /* props.title sets what the title of the popup is
+    *  props.show sets the visibiliy of the popup
+    */
 
     return (
-        <div className="popup" style={{display: popupDisplay}}>
+        <div className="popup">
             {/* Darkens the background */}
             <div style={{position: "fixed", overflowY: "auto", width: "100%", height: "100%", backgroundColor: "rgba(0, 0, 0, 0.5)"}}></div>
 
             {/* Sets the popup element */}
             <div className="popup-frame">
-                <div style={{display: "flex", justifyContent: "space-between", width: "auto", margin: "0 7px"}}>
-                    <p style={{display: "block", fontWeight: "bold"}}>{ props.title }</p>
-                    <a onClick={ (e) => delete_self(e) } className="popup-close">&#10006;</a>
-                </div>
-                <hr />
                 { children }
             </div>
         </div>
     );
 }
+
 export {Popup};
--- src/views/Projects.js
+++ src/views/Projects.js
@@ -1,13 +1,24 @@
+import {useState} from "react";
+
 import {Button, ButtonContainer} from "../components/buttons";
 import {TopBar} from "../components/topBar";
 import {Popup} from "../components/popup.js";
 
 function Projects() {
+
+    const [popupDisplay, setPopupDisplay] = useState(true);
+
     return (
         <>
-            <Popup title="Title from projects!"> 
-                <p>Woah this is a popup!</p>
-            </Popup>
+            { popupDisplay &&
+                <Popup>
+                    <div style={{display: "flex", justifyContent: "space-between", margin: "0 7px 15px 7px"}}>
+                        <p style={{display: "block", fontWeight: "bold"}}>Title from projects!</p>
+                        <a onClick={ () => setPopupDisplay(false) } className="popup-close">&#10006;</a>
+                    </div>
+                    <p>Woah this is a popup!</p>
+                </Popup>
+            }
             <TopBar title="Projects">
                 <Button>Create Project</Button>
                 <Button>Tags</Button>
@@ -15,7 +26,7 @@ function Projects() {
                 <Button>Ajust Duration</Button>
                 <Button>Start/Stop</Button>
             </TopBar>
-            <p>I was thinking here there could be like a form to create another project</p>
+            <p>Here is the text section for projects.</p>
         </>
     );
 }
